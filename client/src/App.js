8902ff63750a203ce35ca8412bea6e20f512da21
Melvin Vasquez <mvasq95@hotmail.com>
2021-03-09 20:09:18 -0500

    basic react-router-dom setup (home, login, register)

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,49 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, 
+  Switch, 
+  Route,
+  Link
+} from 'react-router-dom';
 import './App.css';
 
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import MenuBar from './components/MenuBar';
 
 function App() {
   return (
     <Router>
-      <MenuBar/>
-      <Route exact path='/' component={Home}/>
-      <Route exact path='/login' component={Login}/>
-      <Route exact path='/register' component={Register}/>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/register">
+            <Register />
+          </Route>
+          
+        </Switch>
+      </div>
+
     </Router>
   );
 }
