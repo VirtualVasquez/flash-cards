5e098c3c8e5d538088e0039a8b2ea0948f49d035
Melvin Vasquez <mvasq95@hotmail.com>
2021-04-09 18:11:54 -0400

    single page subject rendering without errors, need to show/add/delete flashcards

--- client/src/pages/SubjectForm/SubjectForm.js
+++ client/src/pages/SubjectForm/SubjectForm.js
@@ -13,50 +13,39 @@ function SubjectForm(props){
     const subjectId=props.match.params.subjectId;
     console.log(subjectId)
 
-    const {data, error} = useQuery(FETCH_SUBJECT_QUERY, {variables:{subjectId}});
-    const newData = data.getSubject;
+    const {data, loading, error} = useQuery(FETCH_SUBJECT_QUERY, {variables:{subjectId}});
     // console.log(JSON.stringify(error, null, 2))
+    if(loading) return 'Loading...';
+    if(error) return `Error! ${error.message}`;
 
     function deletePostCallback() {
         props.history.push('/');
     }
 
-    let subjectMarkup;
-    if(!newData){
-        subjectMarkup = <p>Loading post...</p>
-    }else{
-        const{
-            createdAt,
-            flashCardCount,
-            flashCards,
-            id,
-            title,
-            username
-        } = newData
-    }
-        subjectMarkup=(
-            <Container fluid>
-            <Nav/>
-            <Container >
-            <Form>
-                <h1>{newData.title}</h1>
-                <Button 
-                    variant="success"                    
-                >
-                    + Add new
-                </Button>
-                <Button 
-                    variant="primary" 
-                    type="submit"
-                >
-                    Submit
-                </Button>
-            </Form>
-            </Container>
-        </Container>
-        )
+    const {id, title, username, createdAt, flashCards, flashCardCount } = data.getSubject;
 
-    return subjectMarkup
+    return (
+        <Container fluid>
+        <Nav/>
+        <Container >
+        <Form>
+            <h1>{title}</h1>
+            <SubjectFormGroup/>
+            <Button 
+                variant="success"                    
+            >
+                + Add new
+            </Button>
+            <Button 
+                variant="primary" 
+                type="submit"
+            >
+                Submit
+            </Button>
+        </Form>
+        </Container>
+        </Container>
+    )
     
 }
 
